5127b1c56c35b62d8b40067707d8bfe734ee65d7
Md-Sakib-Hasan-Shawon <mdsakibhasanshawon20@gmail.com>
2026-06-09 09:15:16 +0600

    Done

--- tb/gray_to_bin_tb.sv
+++ tb/gray_to_bin_tb.sv
@@ -12,6 +12,21 @@ module gray_to_bin_tb;
       .bin (bin)
   );
 
-  // YOU CODE HERE
+logic [WIDTH-1:0] expected_bin;
+initial begin
+    for (i=0; i<(1<<WIDTH); i++) begin
+        gray = i^(i>>1)
+        expected_bin = i;
+
+        if (bin !== expected_bin) begin
+            $error("FAIL: Gray = %b Expected = %b Got = %b", gray, expected_bin, bin);
+        end
+    
+    end
+
+    $display("All tests passed");
+    $finish;
+
+end
 
 endmodule
