98459b055cee3271145b591c37403e1aac4b3800
Foez Ahmed <foez.official@gmail.com>
2026-06-09 13:05:02 +0600

    new file:   Makefile modified:   tb/gray_to_bin_tb.sv

--- tb/gray_to_bin_tb.sv
+++ tb/gray_to_bin_tb.sv
@@ -12,21 +12,21 @@ module gray_to_bin_tb;
       .bin (bin)
   );
 
-logic [WIDTH-1:0] expected_bin;
-initial begin
-    for (i=0; i<(1<<WIDTH); i++) begin
-        gray = i^(i>>1)
-        expected_bin = i;
+  logic [WIDTH-1:0] expected_bin;
+  initial begin
+    for (int i = 0; i < (1 << WIDTH); i++) begin
+      gray = i ^ (i >> 1);
+      expected_bin = i;
 
-        if (bin !== expected_bin) begin
-            $error("FAIL: Gray = %b Expected = %b Got = %b", gray, expected_bin, bin);
-        end
-    
+      if (bin !== expected_bin) begin
+        $error("FAIL: Gray = %b Expected = %b Got = %b", gray, expected_bin, bin);
+      end
+
     end
 
     $display("All tests passed");
     $finish;
 
-end
+  end
 
 endmodule
